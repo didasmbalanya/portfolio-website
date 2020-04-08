d5cc4d355f22c5f49cb06bad6402d49195b17286
Didas Mbalanya <didasmbalanya@gmail.com>
2020-04-08 06:26:30 +0200

    initial commit

---
+++ src/components/content-components/profile.component.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Profile extends Component {
+  render() {
+    return (
+      <section className="profile-section">
+        <div className="profile">
+          <h1>your go to guy</h1>
+          <p>
+            well I can write a lot of none sense just to bother you further so
+            please stop listening well I can write a lot of none sense just to
+            bother you further so please stop listening well I can write a lot
+            of none sense just to bother you further so please stop listening
+            well I can write a lot of none sense just to bother you further so
+            please stop listening well I can write a lot of none sense just to
+            bother you further so please stop listening
+          </p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Profile;
